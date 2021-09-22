1bf0485a53efb755e33dd1ebcccb8bdeba535fac
AdamKuemmel <Adamkuemmel@gmail.com>
2021-09-21 22:34:20 -0600

    started server! finished html routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,32 @@
 ////reurire models and libraries
+const db = require("../models");
+const express = require("express");
+const router = express.Router();
 
 //get route for finding all workouts
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      console.log(dbWorkouts);
+      const workouts = dbWorkouts.map((workout) => {
+        console.log({ workout });
+        const duration = workout.exercises.reduce((acc, next) => {
+          return acc + next.duration;
+        }, 0);
+
+        return {
+          totalDuration: duration,
+          ...workout.toObject(),
+        };
+      });
+
+      res.json(workouts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
 
 //put route to update workouts by ID
 
@@ -9,3 +35,4 @@
 //get route
 
 //module export
+module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,24 @@
 //route all the differnet html pages here
 
 //reuire libraries-- router, express- path
+const path = require("path");
+const express = require("express");
+const router = express.Router();
 
 //get route for the html page --index
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 // get route for the exercise page
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 // get route for the stats page
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 ///export router
+module.exports = router;
